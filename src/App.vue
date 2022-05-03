47a807a19f41a68b97cea9e8adef1013d4b0ef9f
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-05-03 20:50:15 -0300

    init: repository initialization

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
