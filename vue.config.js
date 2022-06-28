d145fb1dd229bf031235fdbe29e9c6dd01f9e57e
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-06-27 23:47:22 -0300

    feat: netilify validation

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production'
-        ? `/${ process.env.VUE_APP_URL_PREFIX }`
-        : '/',
     transpileDependencies: true,
     pluginOptions: {}
 })
