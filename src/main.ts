2c8c1756e319a4f7841da4396dd7e28f0e2485fd
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-10-05 16:43:42 -0300

    feat: implement darkmode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import router from "./router";
 import "@/assets/sass/global/global.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "@fortawesome/fontawesome-free/css/all.css";
+// @ts-ignore
+import Darkmode from 'darkmode-js'
 
 const app = createApp(App);
 
@@ -12,3 +14,12 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+window.addEventListener(
+    'load',
+    () => new Darkmode({
+        time: '0',
+        label: '🌓',
+        buttonColorDark: '#22272e'
+    }).showWidget()
+);
