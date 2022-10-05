f7918617e654fe8afe5ba04e9cfbd65066ec6e0f
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2022-10-05 13:14:43 -0300

    build: implement font-awesome as dependency:

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import "@/assets/sass/global/global.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const app = createApp(App);
 
