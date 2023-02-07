970f0e7f811c57cd5b70b96de705cffaa503a74f
Lucas Nepomuceno <lsn.nepomuceno@gmail.com>
2023-02-07 11:42:45 -0300

    chore: change stringable interface to native php implementation

--- src/Contracts/Stringable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace LSNepomuceno\LaravelA1PdfSign\Contracts;
-
-interface Stringable
-{
-    public function __toString(): string;
-}
--- src/Exceptions/ProcessRunTimeException.php
+++ src/Exceptions/ProcessRunTimeException.php
@@ -3,13 +3,13 @@
 namespace LSNepomuceno\LaravelA1PdfSign\Exceptions;
 
 use Exception;
-use LSNepomuceno\LaravelA1PdfSign\Contracts\Stringable;
+use Stringable;
 
 class ProcessRunTimeException extends Exception implements Stringable
 {
     public function __construct(string $reason, int $code = 0, Exception $previous = null)
     {
-        $reason = preg_replace('/[\n\r]/m', '. ', $reason);
+        $reason  = preg_replace('/[\n\r]/m', '. ', $reason);
         $message = "Process runtime error, reason: \"{$reason}\"";
         parent::__construct($message, $code, $previous);
     }
